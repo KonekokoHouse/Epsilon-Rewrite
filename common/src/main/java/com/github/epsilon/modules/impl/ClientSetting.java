a192dd462fc501909c41809917a0e9998304d651
Chen Meng <chenmeng466@outlook.com>
2026-05-15 23:43:17 +0800

    调整 Client Setting 的 Group 排布

--- common/src/main/java/com/github/epsilon/modules/impl/ClientSetting.java
+++ common/src/main/java/com/github/epsilon/modules/impl/ClientSetting.java
@@ -38,11 +38,11 @@ public class ClientSetting extends Module {
     }
 
     private final SettingGroup sgGeneral = settingGroup("General");
+    private final SettingGroup sgAntiCheat = settingGroup("Anti Cheat");
     private final SettingGroup sgAppearance = settingGroup("Appearance");
-    private final SettingGroup sgMainMenu = settingGroup("Main Menu");
     private final SettingGroup sgNotification = settingGroup("Notification");
-    private final SettingGroup sgChat = settingGroup("Chat");
 
+    // General
     public final KeybindSetting guiKeybind = keybindSetting("Gui Keybind", GLFW.GLFW_KEY_RIGHT_SHIFT).group(sgGeneral);
 
     private final ButtonSetting openHudEditor = buttonSetting("Open Hud Editor", () -> mc.setScreen(HudEditorScreen.INSTANCE)).group(sgGeneral);
@@ -53,8 +53,10 @@ public class ClientSetting extends Module {
 
     public final BoolSetting closeOnOutside = boolSetting("Close Gui On Outside", false).group(sgGeneral);
 
-    public final DoubleSetting rotateBackSpeed = doubleSetting("Rotate Back Speed", 5.0f, 1.0f, 10.0f, 0.5f).group(sgGeneral);
+    // Anti Cheat
+    public final DoubleSetting rotateBackSpeed = doubleSetting("Rotate Back Speed", 5.0f, 1.0f, 10.0f, 0.5f).group(sgAntiCheat);
 
+    // Appearance
     public final EnumSetting<ThemeMode> themeMode = enumSetting("Theme Mode", ThemeMode.Dark).group(sgAppearance);
 
     public final EnumSetting<ThemePreset> themePreset = enumSetting("Theme Preset", ThemePreset.Expressive).group(sgAppearance);
@@ -68,20 +70,21 @@ public class ClientSetting extends Module {
 
     public final BoolSetting customTitle = boolSetting("Custom Title", true, _ -> mc.updateTitle()).group(sgAppearance);
 
-    public final BoolSetting useMainMenu = boolSetting("Use MainMenu", true).group(sgMainMenu);
+    public final BoolSetting useMainMenu = boolSetting("Use MainMenu", true).group(sgAppearance);
 
-    public final EnumSetting<MainMenuScreen.Background> mainMenuBackground = enumSetting("MainMenu Background", MainMenuScreen.Background.PLANET, useMainMenu::getValue).group(sgMainMenu);
+    public final EnumSetting<MainMenuScreen.Background> mainMenuBackground = enumSetting("MainMenu Background", MainMenuScreen.Background.PLANET, useMainMenu::getValue).group(sgAppearance);
 
+    // Notification
     public final BoolSetting soundNotify = boolSetting("Sound Notify", true).group(sgNotification);
 
     public final BoolSetting chatNotify = boolSetting("Chat Notify", true).group(sgNotification);
 
-    public final BoolSetting animatedChatPrefix = boolSetting("Animated Chat Prefix", true).group(sgChat);
+    public final BoolSetting animatedChatPrefix = boolSetting("Animated Chat Prefix", true).group(sgNotification);
 
-    public final ColorSetting chatPrefixColorStart = colorSetting("Chat Prefix Color Start", new Color(255, 175, 210), animatedChatPrefix::getValue).group(sgChat);
+    public final ColorSetting chatPrefixColorStart = colorSetting("Chat Prefix Color Start", new Color(255, 175, 210), animatedChatPrefix::getValue).group(sgNotification);
 
-    public final ColorSetting chatPrefixColorEnd = colorSetting("Chat Prefix Color End", new Color(150, 220, 255), animatedChatPrefix::getValue).group(sgChat);
+    public final ColorSetting chatPrefixColorEnd = colorSetting("Chat Prefix Color End", new Color(150, 220, 255), animatedChatPrefix::getValue).group(sgNotification);
 
-    public final DoubleSetting chatPrefixGradientSpeed = doubleSetting("Chat Prefix Gradient Speed", 0.5, 0.1, 1, 0.1, animatedChatPrefix::getValue).group(sgChat);
+    public final DoubleSetting chatPrefixGradientSpeed = doubleSetting("Chat Prefix Gradient Speed", 0.5, 0.1, 1, 0.1, animatedChatPrefix::getValue).group(sgNotification);
 
 }
