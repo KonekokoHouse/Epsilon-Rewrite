0d1bf86394acec7390937b605b9bf41af680f698
L3MonKe <hhcrystal2008@gmail.com>
2026-05-15 22:21:52 +0800

    回滚 HotbarManager

--- common/src/main/java/com/github/epsilon/modules/impl/render/HandsView.java
+++ common/src/main/java/com/github/epsilon/modules/impl/render/HandsView.java
@@ -1,7 +1,5 @@
 package com.github.epsilon.modules.impl.render;
 
-import com.github.epsilon.events.bus.EventHandler;
-import com.github.epsilon.events.impl.PlayerTickEvent;
 import com.github.epsilon.modules.Category;
 import com.github.epsilon.modules.Module;
 import com.github.epsilon.modules.impl.combat.KillAura;
@@ -38,8 +36,8 @@ public class HandsView extends Module {
         Down
     }
 
-    private final BoolSetting disableSwapMain = boolSetting("Disable Swap Main", true);
-    private final BoolSetting disableSwapOff = boolSetting("Disable Swap Off", true);
+    public final BoolSetting disableSwapMain = boolSetting("Disable Swap Main", true);
+    public final BoolSetting disableSwapOff = boolSetting("Disable Swap Off", true);
 
     public final BoolSetting blockingAnimation = boolSetting("Blocking Animation", true);
     public final EnumSetting<BlockMode> blockingMode = enumSetting("Blocking Mode", BlockMode.Pushdown, blockingAnimation::getValue);
@@ -51,19 +49,6 @@ public class HandsView extends Module {
 
     public final BoolSetting swingWhileUsing = boolSetting("Visual Swing On Use", true);
     public final BoolSetting onlyOnBlock = boolSetting("Only On Block", true, swingWhileUsing::getValue);
-
-    @EventHandler
-    private void onPlayerTick(PlayerTickEvent.Pre event) {
-        if (disableSwapMain.getValue() && mc.getEntityRenderDispatcher().getItemInHandRenderer().mainHandHeight <= 1f) {
-            mc.getEntityRenderDispatcher().getItemInHandRenderer().mainHandHeight = 1f;
-            mc.getEntityRenderDispatcher().getItemInHandRenderer().mainHandItem = mc.player.getMainHandItem();
-        }
-
-        if (disableSwapOff.getValue() && mc.getEntityRenderDispatcher().getItemInHandRenderer().offHandHeight <= 1f) {
-            mc.getEntityRenderDispatcher().getItemInHandRenderer().offHandHeight = 1f;
-            mc.getEntityRenderDispatcher().getItemInHandRenderer().offHandItem = mc.player.getOffhandItem();
-        }
-    }
 
     public boolean isBlocking() {
         KillAura killAura = KillAura.INSTANCE;
